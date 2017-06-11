d00ac2d2eabe57b23f75c5cf2cbfb4e8504e87ee
Ludo van den Boom <ludo@cubicphuse.nl>
2017-06-11 14:34:07 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# TorchDemo
+
+React Native sample application using [react-native-torch](https://github.com/ludo/react-native-torch)
+plugin.
+
+Keep in mind that this demo will only work on real devices. The simulators have
+no torch so it won't work there.
+
+## Usage
+
+First, clone this repository. Then:
+
+### Android
+
+```shell
+react-native run-android
+```
+
+### iOS
+
+1. Open `ios/TorchDemo.xcodeproj` in XCode
+2. Select development team for code-signing
+3. Run!
+
---
+++ index.ios.js
@@ -0,0 +1,66 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  Button,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+import Torch from 'react-native-torch';
+
+export default class TorchDemo extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isTorchOn: false,
+    };
+  }
+
+  _handlePress() {
+    const { isTorchOn } = this.state;
+    Torch.switchState(!isTorchOn);
+    this.setState({ isTorchOn: !isTorchOn });
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          RCTTorch Demo
+        </Text>
+        <Button
+          onPress={this._handlePress.bind(this)}
+          title="Toggle Torch"
+        />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('TorchDemo', () => TorchDemo);
